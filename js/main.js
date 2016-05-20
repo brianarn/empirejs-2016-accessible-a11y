0ca2ff99aa1443c07a0f6bd57950e5beb00ca5f5
Brian Sinclair <brianarn@gmail.com>
2016-05-20 00:47:35 -0600

    Small adjustments to presentation
    - Added a loading screen of sorts
    - Hid the default footer, removing that display attribute pre-clone
    - Added comments through the main JS

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 (function () {
+  // Set up to fetch our presentation content
+  // (which I could have inlined in the HTML but this made authoring easier)
   var xhr = new XMLHttpRequest();
+
   xhr.addEventListener('load', () => {
+    // Pull the markdown for the presentation as plain text
     var markdown = xhr.responseText;
+
+    // Yank our loading slide
+    document.querySelector('#loading').remove();
+
+    // Get our slide footer from the page, pulling it out of the DOM and making
+    // it display (as it was hidden originally)
+    var slideFooter = document.querySelector('.slide-footer');
+    slideFooter.parentElement.removeChild(slideFooter);
+    slideFooter.style.display = '';
 
     // Create the slideshow
     var slideshow = window.slideshow = remark.create({
@@ -13,18 +26,20 @@
       slideNumberFormat: ''
     });
 
-    // Get our slide footer from the page, and inject a copy of it all over
-    var slideFooter = document.querySelector('.slide-footer');
-    slideFooter.parentElement.removeChild(slideFooter);
+    // Now that the presentation markup is there, inject clones of our slide
+    // footer into all of the content nodes.
     document.querySelectorAll('.remark-slide-content').forEach(slide => {
       slide.appendChild(slideFooter.cloneNode(true));
     });
 
-    // Make it ARIA live
+    // Make it ARIA live, so that it'll read as users navigate.
+    // Maybe 'assertive' is too strong, but it means you can quickly skip back
+    // and forth during the reading.
     var slideArea = document.querySelector('.remark-slides-area');
     slideArea.setAttribute('aria-live', 'assertive');
   });
 
+  // Actually grab the content now that we're ready to handle it.
   xhr.open('GET', 'md/presentation.md');
   xhr.send();
 })();
